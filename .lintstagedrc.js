4dde9b64d60acc20307da4c933326f0d8ea75283
Vinicios Oliveira <vinicios.oliveira@pitzi.com.br>
2023-09-25 21:02:45 -0300

    fix: fix prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,9 @@
 const path = require('path')
 
-const buildEslintCommand = (filenames) => [
-  `npm run lint --fix . ${filenames.join(' --file')}`,
+const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
-]
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand]
