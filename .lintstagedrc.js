5193a8e413655f75899d0593a14f80fad5ea9df1
Vinicios Oliveira <vinicios.oliveira@pitzi.com.br>
2023-09-25 22:16:24 -0300

    foo

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,7 @@ const buildEslintCommand = (filenames) => [
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`,
-  `npm test -- -- findRelatedTests ${filenames.join(' ')}`
+  `npm run test -- -- findRelatedTests ${filenames.join(' ')}`
 ]
 
 module.exports = {
